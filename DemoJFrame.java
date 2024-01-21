78f81654268d241df9dc6526a24f1b8bf14c628f
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-01-21 19:09:31 +0530

    DemoJFrame

---
+++ DemoJFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class DemoJFrame extends JFrame{
+    DemoJFrame()
+    {
+        setLayout(null);
+        setSize(400,400);
+        setVisible(true);
+        setTitle("Demo JFrame");
+    }
+    public static void main(String[] args) {
+        DemoJFrame DJ = new DemoJFrame();
+    }
+}
