b6b3e17e2781bd1b20d9b84b7b15d5727484fa6a
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-01-25 18:33:09 +0530

    Swing Within Main Function

--- SimpleSwing.java
+++ SimpleSwing.java
@@ -2,14 +2,14 @@ import javax.swing.*;
 public class SimpleSwing {
     public static void main(String args[]){
         JFrame jf = new JFrame();
-        JLabel jb = new JLabel("Hello Swing");
+        JLabel jl = new JLabel("Hello Swing");
 
         jf.setSize(400,400);
         jf.setTitle("Simple Swing");
         jf.setLayout(null);
-        jb.setBounds(150, 150, 80, 60);
+        jl.setBounds(150, 150, 80, 60);
         jf.setVisible(true);
-        jf.add(jb);
+        jf.add(jl);
     }
     
 }
