1d68317c41876eb0da681117253fc929f1ce8dc2
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-01-26 16:54:59 +0530

    Swing With Inheritance

--- InheritSwing.java
+++ InheritSwing.java
@@ -1,12 +1,10 @@
 import javax.swing.*;
-import java.awt.*;
 public class InheritSwing extends JFrame {
     JLabel jl;
     InheritSwing(){
         jl = new JLabel("Hello Swing");
-        jl.setBounds(100,100,80,80);
+        jl.setBounds(150,100,80,80);
         setVisible(true);
-        // setTitle(getTitle());
         setSize(400,400);
         setLayout(null);
         add(jl);
