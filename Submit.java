89c0d08660d7f8553b99c805e017864386b318d8
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-01-28 22:51:03 +0530

    Submit Form

--- Submit.java
+++ Submit.java
@@ -9,15 +9,17 @@ public class Submit extends JFrame{
         b = new JButton("Submit");
         l=new JLabel("Enter Your Name");
         t=new JTextField(10);
-        b.setBounds(50, 50, 80, 30);
+        b.setBounds(150, 200, 80, 30);
         l.setBounds(130, 100, 150, 30);
-        t.setBounds(80, 80, 200, 30);
+        t.setBounds(90, 150, 200, 30);
         
-
+        setTitle("Submit Form");
         setVisible(true);
         setSize(400,400);
         setLayout(null);
         add(l);
+        add(t);
+        add(b);
 
     }
     public static void main(String[] args) {
