3b37a1718be7ea89d9fb6405e2149412a02b94ba
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-01 23:04:35 +0530

    JPasswordField

--- Password.java
+++ Password.java
@@ -2,18 +2,22 @@ import javax.swing.*;
 public class Password extends JFrame{
     JPasswordField p;
     JLabel l;
+    JButton b;
     Password()
     {
+        b = new JButton("Login");
         p = new JPasswordField("Admin");
         l = new JLabel("Password");
+        b.setBounds(20,160 , 80, 20);
         l.setBounds(20, 50, 80, 80);
-        p.setBounds(20, 120, 80, 20);
+        p.setBounds(20, 120, 100, 20);
         setSize(300,300);
         setLayout(null);
         setVisible(true);
         setTitle("JPassword");
         add(l);
         add(p);
+        add(b);
     }
     public static void main(String[] args) {
         new Password();
