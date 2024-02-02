292cf367d1b39491b278033b2564e812c8df8175
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-02 21:57:32 +0530

    JButton

---
+++ DemoJButton.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class DemoJButton extends JFrame {
+    JButton b;
+
+    public DemoJButton() {
+        b = new JButton("Click Me");
+        b.setBounds(81, 90, 100, 30);
+        setSize(300, 300);
+        setLayout(null);
+        setVisible(true);
+        setTitle("Demo JButton");
+        add(b);
+    }
+
+    public static void main(String[] args) {
+        new DemoJButton();
+    }
+}
