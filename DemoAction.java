e305932804b9d203e193343d7a488f7481d3f061
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-07 23:08:51 +0530

    Test ActionListener

--- DemoAction.java
+++ DemoAction.java
@@ -1,19 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class DemoAction extends JFrame {
+public class DemoAction extends JFrame implements  ActionListener {
     JButton b;
     JLabel l;
     JTextField t;
     DemoAction(){
-        b = new JButton();
+        b = new JButton("Click Me");
         l = new JLabel("Label");
         t = new JTextField(10);
         b.setBounds(80,80,100,20);
         l.setBounds(80,120,100,30);
         t.setBounds(80,30,100,20);
         
-        
+        b.addActionListener(this);
         
         setSize(300,300);
         setVisible(true);
@@ -25,6 +25,10 @@ public class DemoAction extends JFrame {
         add(t);
 
 
+    }
+    public void actionPerformed (ActionEvent e) {
+        l.setText("Hello Label");
+        t.setText("Hello TextField");
     }
     public static void main(String[] args) {
         new DemoAction();
