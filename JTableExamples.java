d542a9651abafe8f7b8790f5add9b94ba2a803e1
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-12 13:31:44 +0530

    Add files via upload
    JTable

---
+++ JTableExamples.java
@@ -0,0 +1,29 @@
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class JTableExamples {
+    JFrame f;
+    JTable j;
+
+    JTableExamples() {
+        f = new JFrame();
+        f.setTitle("JTable Example");
+        // Data to be displayed in the JTable
+        String[][] data = { { "Kundan Kumar Jha", "4031", "CSE" }, { "Anand Jha", "6014", "IT" } };
+        // Column Names
+        String[] columnNames = { "Name", "Roll Number", "Department" };
+        // Initializing the JTable
+        j = new JTable(data, columnNames);
+        j.setBounds(30, 40, 200, 300);
+        // adding it to JScrollPane
+        JScrollPane sp = new JScrollPane(j);
+        f.add(sp);
+        f.setSize(500, 200);
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new JTableExamples();
+    }
+}
