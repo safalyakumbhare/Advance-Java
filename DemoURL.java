9cc8d6cd97181a1e412453ac483921a593c7e8aa
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-02-13 13:31:15 +0530

    Demo URL

---
+++ DemoURL.java
@@ -0,0 +1,19 @@
+import java.net.*;
+class DemoURL
+{
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http","www.yahoo.com","/index.html");
+            System.out.println("Protocol "+url.getProtocol());
+            System.out.println("Host : "+url.getHost());
+            System.out.println("Port : "+url.getPort());
+            System.out.println("File : "+url.getFile());
+           
+            
+
+        } catch (Exception e) {
+            System.out.println(e);
+            // TODO: handle exception
+        }
+    }
+}
