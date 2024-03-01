6b2753e230a0521679636bb5ec296a7b6e4ff272
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-01 23:10:12 +0530

    INet Address

--- DemoInetAddress.java
+++ DemoInetAddress.java
@@ -3,9 +3,9 @@ class DemoInetAddress{
     public static void main(String[] args) {
         try {
             InetAddress obj = InetAddress.getLocalHost();
-            // System.out.println("Host Name : "+obj.getLocalHost());
-            // System.out.println("Host Address : "+obj.getHostAddress());
-            // System.out.println("Host Details: "+obj.toString());
+            System.out.println("Host Name : "+obj.getLocalHost());
+            System.out.println("Host Address : "+obj.getHostAddress());
+            System.out.println("Host Details: "+obj.toString());
             
         } catch (Exception e) {
             // TODO: handle exception
