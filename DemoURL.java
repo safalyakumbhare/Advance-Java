5dfe95dab348044a5172b3c3a53a77c6cbd32b3d
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-03-06 22:47:35 +0530

    DemoURL

--- DemoURL.java
+++ DemoURL.java
@@ -4,13 +4,10 @@ class DemoURL
     public static void main(String[] args) {
         try {
             URL url = new URL("http","www.yahoo.com",60,"/index.html");
-            System.out.println("Protocol "+url.getProtocol());
+            System.out.println("Protocol : "+url.getProtocol());
             System.out.println("Host : "+url.getHost());
             System.out.println("Port : "+url.getPort());
             System.out.println("File : "+url.getFile());
-           
-            
-
         } catch (Exception e) {
             System.out.println(e);
             // TODO: handle exception
