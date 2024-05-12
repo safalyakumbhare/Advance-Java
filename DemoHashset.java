1e0d992f1fcd8e2ec6ec52128cdbb05c4e596795
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-05-12 14:37:55 +0530

    HashSet

---
+++ DemoHashset.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class DemoHashset {
+    public static void main(String[] args) {
+        HashSet<String> name = new HashSet<>();
+        name.add("Prajakta");
+        name.add("Safalya");
+        name.add("Abhay");
+        name.add("Vaishnavi");
+        name.add("Vaishnavi");
+        System.out.println(name);
+    }
+}
