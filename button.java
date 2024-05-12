519cbc901d260d06648ea6ee1b7442837232f6e6
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-05-12 16:58:19 +0530

    Jbutton

--- button.java
+++ button.java
@@ -5,7 +5,7 @@ class button extends JFrame
     button()
     {
         pqr=new JButton("Hello User");
-        pqr.setBounds (100,100,200,300);
+        pqr.setBounds (50,50,200,30);
         setVisible(true);
         setLayout(null);
         setSize(300,200);
@@ -13,7 +13,7 @@ class button extends JFrame
     add(pqr);
     }
     public static void main(String[] args) {
-        textfield pqr=new textfield();
+        new button();
     }
     
 }
