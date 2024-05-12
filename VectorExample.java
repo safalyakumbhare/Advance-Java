4e98613f5f2b34aceaed090e4e2c1ab275316556
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-05-12 23:16:00 +0530

    Vector class

---
+++ VectorExample.java
@@ -0,0 +1,10 @@
+import java.util.Vector;
+public class VectorExample {
+    public static void main(String[] args) {
+        Vector<Integer> numbers = new Vector<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        System.out.println("Vector elements: "+numbers);
+    }
+}
