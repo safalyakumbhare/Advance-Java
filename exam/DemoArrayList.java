0a24cca8b98a65ac159438c3bcb9fef3818f2d79
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-05-22 23:08:59 +0530

    Arraylist

---
+++ exam/DemoArrayList.java
@@ -0,0 +1,12 @@
+package exam;
+import java.util.*;
+class DemoArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> name = new ArrayList<>();
+        name.add("Asus");
+        name.add("Dell");
+        name.add("HP");
+        name.add(2,"d");
+        System.out.println(name);
+    }
+}
